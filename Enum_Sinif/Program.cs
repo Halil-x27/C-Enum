00bd2a21c77f1d8b1eb6be1209b502e9042a76cf
Halil Özcan <131258476+Halil-x27@users.noreply.github.com>
2024-07-10 17:42:55 +0300

    Add files via upload

---
+++ Enum_Sinif/Program.cs
@@ -0,0 +1,49 @@
+﻿using System;
+
+namespace MyNamespace
+{
+    class Program
+    {
+        public static void Main(string[] args)
+        {
+            Console.WriteLine(Gunler.pazar);
+            Console.WriteLine((int)Gunler.cumartesi);
+
+            int sıcaklık = 32;
+
+            if (sıcaklık<=(int)HavaDurumu.normal)
+            {
+                Console.WriteLine("Dısarıya çıkmak için biraz daha bekleyelim");
+            }
+            else if (sıcaklık>=(int)HavaDurumu.sıcak)
+            {
+                Console.WriteLine("dısarıya çıkmak için sıcak bir gün");
+            }else if (sıcaklık>=(int)HavaDurumu.normal && sıcaklık<(int)HavaDurumu.cokSıcak)
+            {
+                Console.WriteLine("dısarıya çıkmak için güzel bir gün");
+            }
+        }
+    }
+
+    enum Gunler
+    {
+        pazartesi,
+        sali,
+        carsamba,
+        persembe,
+        cuma,
+        cumartesi,
+        pazar
+    }
+
+    enum HavaDurumu
+    {
+        soguk = 5,
+
+        normal = 20,
+
+        sıcak = 25,
+
+        cokSıcak = 30
+    }
+}
